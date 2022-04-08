7f9b9024017e34a8e9ce43ac5185606a6659b9e7
rbromley19 <ua19949@bristol.ac.uk>
2022-04-08 18:22:22 +0100

    Update predatorPrey.py
    added predator-prey plot

--- predatorPrey.py
+++ predatorPrey.py
@@ -1,22 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
+from matplotlib import pyplot
+from solveODE import solve_ode
+from scipy.optimize import fsolve
 
 
-def pred_prey(t, u, a, b, d):
+def pred_prey(t, u):
+    a = 1
+    b = 0.28
+    d = 0.1
     x, y = u
     dx = x * (1 - x) - (a * x * y) / (d + x)
     dy = b * y * (1 - (y / x))
     return np.array((dx, dy))
 
 
-sol = solve_ivp(pred_prey, [0, 15], [10, 5], args=(1, 0.26, 0.1),
-                dense_output=True)
-t = np.linspace(0, 15, 300)
-z = sol.sol(t)
+int = [0.5, 0.5]
+t = np.linspace(0, 200, 2001)
+bspace = np.linspace(0.1, 0.5, 5)
 
-plt.plot(t, z.T)
-plt.xlabel('t')
-plt.legend(['x', 'y'], shadow=True)
-plt.title('Predator-Prey System')
+sol = solve_ode(pred_prey, int, t, 'rk4', 0.001, 0)
+print(sol)
+x = sol[:, 0]
+y = sol[:, 1]
+# phase portrait: x, time-series: t
+plt.plot(x, y)
+plt.xlabel('x')
+plt.ylabel('y')
 plt.show()
+
+
+
+# sol = solve_ivp(pred_prey, [0, 15], [10, 5], dense_output=True)
+#
+# z = sol.sol(t)
+#
+# pyplot.plot(sol.t, sol.y[0, :])
+# pyplot.show()
