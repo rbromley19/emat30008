2db0e0366610591917956f16254106609361a49a
rbromley19 <ua19949@bristol.ac.uk>
2022-04-13 17:01:16 +0100

    added search for equilibrium

--- predator_prey.py
+++ predator_prey.py
@@ -22,17 +22,13 @@ t = np.linspace(0, 150, 1501)
 bspace = np.linspace(0.1, 0.5, 5)
 
 sol = solve_ode(lambda t, u: pred_prey(t, u, b), int, t, 'rk4', 0.001)
-print(sol)
 x = sol[:, 0]
 y = sol[:, 1]
-# phase portrait: x, time-series: t
-plt.plot(x, y)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.show()
+pyplot.plot(x, y)
+pyplot.show()
 
 # X-nullcline
-Xval = np.linspace(0.05,0.55, 51)
+Xval = np.linspace(0.1, 0.6, 100)
 Yval = np.zeros(np.size(Xval))
 for (i, X) in enumerate(Xval):
     result = root(lambda N: pred_prey(nan, (X, N), b)[0], 0.25)
@@ -43,7 +39,7 @@ for (i, X) in enumerate(Xval):
 pyplot.plot(Xval, Yval)
 
 # Y-nullcline
-Xval = np.linspace(0.05, 0.55, 51)
+Xval = np.linspace(0.1, 0.6, 100)
 Yval = np.zeros(np.size(Xval))
 for (i, X) in enumerate(Xval):
     result = root(lambda N: pred_prey(nan, (X, N), b)[1], 0.25)
@@ -52,3 +48,21 @@ for (i, X) in enumerate(Xval):
     else:
         Yval[i] = nan
 pyplot.plot(Xval, Yval)
+pyplot.show()
+
+# %%
+result = root(lambda u: pred_prey(nan, u, b), (0.25, 0.25))
+if result.success:
+    print("Equilibrium at {}".format(result.x))
+else:
+    print("Failed to converge")
+result = root(lambda u: pred_prey(nan, u, b), (0.5, 0.3))
+if result.success:
+    print("Equilibrium at {}".format(result.x))
+else:
+    print("Failed to converge")
+# result = root(lambda u: pred_prey(nan, u, b), np.array((0.4, 0.5)))
+# if result.success:
+#     print("Equilibrium at {}".format(result.x))
+# else:
+#     print("Failed to converge")
