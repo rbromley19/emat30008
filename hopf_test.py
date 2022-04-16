3835e2430e55026d4bcfbb98412051c15aa8279f
rbromley19 <ua19949@bristol.ac.uk>
2022-04-16 15:11:34 +0100

    added function for hopf testing to remove hard coded testing parameters, added comments to hopf_test file

--- hopf_test.py
+++ hopf_test.py
@@ -2,6 +2,7 @@ import numerical_shooting
 import numpy as np
 
 
+# Function to simulate Hopf bifurcation normal form
 def hopf_bf(t, u, b):
     s = -1
     u1, u2 = u
@@ -10,28 +11,30 @@ def hopf_bf(t, u, b):
     return np.array([du1, du2])
 
 
+# Function to simulate Hopf bifurcation explicit solution
 def hopf_exp(t, theta, b):
     u1 = np.sqrt(b) * np.cos(t + theta)
     u2 = np.sqrt(b) * np.sin(t + theta)
     return np.array([u1, u2])
 
 
-# sol = solve_ode(lambda t, u: hopf_bf(t, u, b=0.5), (-1, 0), t, 'rk4', 0.001)
-# x = sol[:, 0]
-# y = sol[:, 1]
-# pyplot.plot(x, y)
-# pyplot.show()
+# Function to test Hopf bifurcation shooting result against explicit solution
+def hopf_test(b, u0, T0, phase0, t):
+    shoot_result = numerical_shooting.orbit_calc(lambda t, u: hopf_bf(t, u, b=b), u0, T0, phase0)
+    shoot_points = shoot_result[:-1]
+    T = shoot_result[-1]
+    exp_result = hopf_exp(t, theta=T, b=b)
+    print(exp_result)
+    if np.allclose(exp_result, shoot_points):
+        print("pass")
+    else:
+        print("fail")
 
-result = numerical_shooting.orbit_calc(lambda t, u: hopf_bf(t, u, b=1), [1, 1.5], 5, 0)
-point = result[:-1]
-T = result[-1]
 
-# sol = solve_ode(lambda t, u: hopf_bf(t, u, b=0.5), (-1, 0), t, 'rk4', 0.001)
-result_exp = hopf_exp(0, theta=T, b=1)
-print(result_exp)
-
-if np.allclose(result_exp, point):
-    print("pass")
-else:
-    print("fail")
-
+if __name__ == '__main__':
+    b = 1
+    u0 = [1, 1.5]
+    T0 = 5
+    phase0 = 0
+    t = 0
+    hopf_test(b, u0, T0, phase0, t)
