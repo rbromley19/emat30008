f0585ef4cf0346ecb43345fd5eaece6511229d3f
rbromley19 <ua19949@bristol.ac.uk>
2022-04-16 16:10:02 +0100

    Added 3rd dimension system (eq. 3 from code testing) and respective test function (not working yet)

--- hopf_test.py
+++ hopf_test.py
@@ -1,21 +1,6 @@
 import numerical_shooting
 import numpy as np
-
-
-# Function to simulate Hopf bifurcation normal form
-def hopf_bf(t, u, b):
-    s = -1
-    u1, u2 = u
-    du1 = b * u1 - u2 + s * u1 * (u1 ** 2 + u2 ** 2)
-    du2 = u1 + b * u2 + s * u2 * (u1 ** 2 + u2 ** 2)
-    return np.array([du1, du2])
-
-
-# Function to simulate Hopf bifurcation explicit solution
-def hopf_exp(t, theta, b):
-    u1 = np.sqrt(b) * np.cos(t + theta)
-    u2 = np.sqrt(b) * np.sin(t + theta)
-    return np.array([u1, u2])
+from ode_functions import hopf_bf, hopf_exp, ode_3, ode_3_exp
 
 
 # Function to test Hopf bifurcation shooting result against explicit solution
@@ -31,10 +16,26 @@ def hopf_test(b, u0, T0, phase0, t):
         print("fail")
 
 
+def ode_3(t, u, b, s):
+    u1, u2, u3 = u
+    du1 = b * u1 - u2 + s * u1 * (u1 ** 2 + u2 ** 2)
+    du2 = u1 + b * u2 + s * u2 * (u1 ** 2 + u2 ** 2)
+    du3 = -u3
+    return np.array([du1, du2, du3])
+
+
+def du3_test(b, s, u0, T0, phase0, t):
+    shoot_result = numerical_shooting.orbit_calc(lambda t, u: ode_3(t, u, b=b, s=s), u0, T0, phase0)
+    shoot_points = shoot_result[:-1]
+    T = shoot_result[-1]
+    exp_result = hopf_exp(t, theta=T, b=b, s=s)
+    print(exp_result)
+    if np.allclose(exp_result, shoot_points):
+        print("pass")
+    else:
+        print("fail")
+
+
 if __name__ == '__main__':
-    b = 1
-    u0 = [1, 1.5]
-    T0 = 5
-    phase0 = 0
-    t = 0
-    hopf_test(b, u0, T0, phase0, t)
+    hopf_test(1, [1, 1.5], 5, 0, 0)
+    du3_test(1, -1, [1.5, 0.5, 1], 5, 0, 0)
--- ode_functions.py
+++ ode_functions.py
@@ -1,10 +1,12 @@
 import numpy as np
 
 
+# Function to simulate ODE x. = x
 def ode_1(t, x):
     return x
 
 
+# Function to simulate ODE x.. = -x
 def f_s(t, X):
     x = X[0]
     y = X[1]
@@ -13,7 +15,51 @@ def f_s(t, X):
     return np.array((xdot, ydot))
 
 
+# Function to simulate analytic solution to f_s
 def X_analytic(t, X0):
     c2, c1 = X0
-
     return [c1 * np.sin(t) + c2 * np.cos(t), c1 * np.cos(t) - c2 * np.sin(t)]
+
+
+# Function to simulate predator-prey equations
+def pred_prey(t, u, b):
+    a = 1
+    d = 0.1
+    b = 0.2
+    x, y = u
+    dx = x * (1 - x) - (a * x * y) / (d + x)
+    dy = b * y * (1 - (y / x))
+    return np.array((dx, dy))
+
+
+# Function to simulate Hopf bifurcation normal form
+def hopf_bf(t, u, b):
+    s = -1
+    u1, u2 = u
+    du1 = b * u1 - u2 + s * u1 * (u1 ** 2 + u2 ** 2)
+    du2 = u1 + b * u2 + s * u2 * (u1 ** 2 + u2 ** 2)
+    return np.array([du1, du2])
+
+
+# Function to simulate Hopf bifurcation explicit solution
+def hopf_exp(t, theta, b):
+    u1 = np.sqrt(b) * np.cos(t + theta)
+    u2 = np.sqrt(b) * np.sin(t + theta)
+    return np.array([u1, u2])
+
+
+# Function to simulate third order system of ODEs (eq. 3 in code testing)
+def ode_3(t, u, b, s):
+    u1, u2, u3 = u
+    du1 = b * u1 - u2 + s * u1 * (u1 ** 2 + u2 ** 2)
+    du2 = u1 + b * u2 + s * u2 * (u1 ** 2 + u2 ** 2)
+    du3 = -u3
+    return np.array([du1, du2, du3])
+
+
+# Function to simulate explicit solution to third order system of ODEs (eq. 3 in code testing)
+def ode_3_exp(t, theta, b):
+    u1 = np.sqrt(b) * np.cos(t + theta)
+    u2 = np.sqrt(b) * np.sin(t + theta)
+    u3 = np.exp(-(t + theta))
+    return np.array([u1, u2, u3])
