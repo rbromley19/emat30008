d1dc7ac374c576f2a2d465c03c8210ebea7cc53d
rbromley19 <ua19949@bristol.ac.uk>
2022-04-25 09:54:50 +0100

    added more docstrings

--- ode_functions.py
+++ ode_functions.py
@@ -3,11 +3,39 @@ import numpy as np
 
 # Function to simulate ODE x. = x
 def ode_1(t, x):
+    """Function to simulate the ODE x' = x
+
+Parameters
+----------
+t  : float
+     Time
+x  : array
+     x-value at time t
+
+Returns
+-------
+x : ODE simulation
+"""
+
     return x
 
 
 # Function to simulate ODE x.. = -x
 def f_s(t, X):
+    """Function to simulate the ODE x.. = -x
+
+Parameters
+----------
+t  : float
+     Time
+x  : array
+     x-value at time t
+
+Returns
+-------
+Array of dx/dt and dy/dt
+"""
+
     x = X[0]
     y = X[1]
     xdot = y
@@ -17,12 +45,41 @@ def f_s(t, X):
 
 # Function to simulate analytic solution to f_s
 def X_analytic(t, X0):
+    """Function to simulate the analytic solution to f_s
+
+Parameters
+----------
+t  : float
+     Time
+X0  : array
+     Initial x conditions
+
+Returns
+-------
+Array of the analytic solution to f_s
+"""
+
     c2, c1 = X0
     return [c1 * np.sin(t) + c2 * np.cos(t), c1 * np.cos(t) - c2 * np.sin(t)]
 
 
 # Function to simulate predator-prey equations
 def predator_prey(t, u, b):
+    """Function to simulate the predator-prey (Lokterra-Volterra Equations)
+
+Parameters
+----------
+t  : float
+     Time
+u  : array
+     x-value at time t
+b  : float
+     value of constant 'b'
+Returns
+-------
+Array of dx/dt and dy/dt
+"""
+
     a = 1
     d = 0.1
     b = 0.2
@@ -34,6 +91,21 @@ def predator_prey(t, u, b):
 
 # Function to simulate Hopf bifurcation normal form
 def hopf_bf(t, u, b):
+    """Function to simulate the Hopf bifurcation normal form
+
+Parameters
+----------
+t  : float
+     Time
+u  : array
+     x-value at time t
+b  : float
+     value of constant 'b'
+Returns
+-------
+Array of du1/dt and du2/dt
+"""
+
     s = -1
     u1, u2 = u
     du1 = b * u1 - u2 + s * u1 * (u1 ** 2 + u2 ** 2)
@@ -43,6 +115,21 @@ def hopf_bf(t, u, b):
 
 # Function to simulate Hopf bifurcation explicit solution
 def hopf_exp(t, theta, b):
+    """Function to simulate the Hopf bifurcation explicit solution
+
+Parameters
+----------
+t      : float
+         Time
+theta  : float
+         phase
+b      : float
+         value of constant 'b'
+Returns
+-------
+Array of u1 and u2
+"""
+
     u1 = np.sqrt(b) * np.cos(t + theta)
     u2 = np.sqrt(b) * np.sin(t + theta)
     return np.array([u1, u2])
@@ -50,6 +137,24 @@ def hopf_exp(t, theta, b):
 
 # Function to simulate third order system of ODEs (eq. 3 in code testing)
 def ode_3(t, u, b, s):
+    """Function to simulate the third order system of ODEs (eq. 3 in code testing)
+
+Parameters
+----------
+t  : float
+     Time
+u  : array
+     x-value at time t
+b  : float
+     value of constant 'b'
+s  : float
+     value of constant 'b'
+
+Returns
+-------
+Array of du1/dt, du2/dt and du3/dt
+"""
+
     u1, u2, u3 = u
     du1 = b * u1 - u2 + s * u1 * (u1 ** 2 + u2 ** 2)
     du2 = u1 + b * u2 + s * u2 * (u1 ** 2 + u2 ** 2)
@@ -59,6 +164,22 @@ def ode_3(t, u, b, s):
 
 # Function to simulate explicit solution to third order system of ODEs (eq. 3 in code testing)
 def ode_3_exp(t, theta, b):
+    """Function to simulate the explicit solution to ode_3
+
+Parameters
+----------
+t      : float
+         Time
+theta  : float
+         phase
+b      : float
+        value of constant 'b'
+
+Returns
+-------
+Array of du1/dt, du2/dt and du3/dt
+"""
+
     u1 = np.sqrt(b) * np.cos(t + theta)
     u2 = np.sqrt(b) * np.sin(t + theta)
     u3 = np.exp(-(t + theta))
@@ -66,10 +187,40 @@ def ode_3_exp(t, theta, b):
 
 
 def cubic(x, c):
+    """Function to simulate the cubic equation
+
+Parameters
+----------
+x  : arrat
+     value of variable 'x'
+c  : float
+     value of constant 'c'
+
+Returns
+-------
+Cubic equation
+"""
+
     return x ** 3 - x + c
 
 
 def hopf_normal(t, u, b):
+    """Function to simulate the Hopf normal form
+
+Parameters
+----------
+t  : float
+     Time
+u  : array
+     x-value at time t
+b  : float
+     value of constant 'b'
+
+Returns
+-------
+Array of du1/dt and du2/dt
+"""
+
     u1, u2 = u
     du1 = b * u1 - u2 - u1 * (u1 ** 2 + u2 ** 2)
     du2 = u1 + b * u2 - u2 * (u1 ** 2 + u2 ** 2)
@@ -77,6 +228,22 @@ def hopf_normal(t, u, b):
 
 
 def hopf_mod(t, u, b):
+    """Function to simulate the modified Hopf
+
+Parameters
+----------
+t  : float
+     Time
+u  : array
+     x-value at time t
+b  : float
+     value of constant 'b'
+
+Returns
+-------
+"""
+
     u1, u2 = u
     du1 = b * u1 - u2 + u1 * (u1 ** 2 + u2 ** 2) - u1 * (u1 ** 2 + u2 ** 2) ** 2
     du2 = u1 + b * u2 + u2 * (u1 ** 2 + u2 ** 2) - u2 * (u1 ** 2 + u2 ** 2) ** 2
+
