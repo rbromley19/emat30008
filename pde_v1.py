74e3cca2e538d9d0128b22dc5c6d958eaa9df5d7
rbromley19 <ua19949@bristol.ac.uk>
2022-04-29 14:16:26 +0100

    added amendments to PDEs from pair coding work (q1 - 3)

--- pde_v1.py
+++ pde_v1.py
@@ -8,61 +8,86 @@
 import numpy as np
 import pylab as pl
 from math import pi
+import scipy
+from scipy.sparse.linalg import spsolve
 
 # Set problem parameters/functions
-kappa = 1.0   # diffusion constant
-L=1.0         # length of spatial domain
-T=0.5         # total time to solve for
+kappa = 1.0  # diffusion constant
+L = 1.0  # length of spatial domain
+T = 0.5  # total time to solve for
+
+
 def u_I(x):
     # initial temperature distribution
-    y = np.sin(pi*x/L)
+    y = np.sin(pi * x / L)
     return y
 
-def u_exact(x,t):
+
+def u_exact(x, t):
     # the exact solution
-    y = np.exp(-kappa*(pi**2/L**2)*t)*np.sin(pi*x/L)
+    y = np.exp(-kappa * (pi ** 2 / L ** 2) * t) * np.sin(pi * x / L)
     return y
 
-# Set numerical parameters
-mx = 10     # number of gridpoints in space
-mt = 1000   # number of gridpoints in time
 
-# Set up the numerical environment variables
-x = np.linspace(0, L, mx+1)     # mesh points in space
-t = np.linspace(0, T, mt+1)     # mesh points in time
-deltax = x[1] - x[0]            # gridspacing in x
-deltat = t[1] - t[0]            # gridspacing in t
-lmbda = kappa*deltat/(deltax**2)    # mesh fourier number
-print("deltax=",deltax)
-print("deltat=",deltat)
-print("lambda=",lmbda)
+def solve_PDE(u_I, kappa, L, T, mx, mt, method):
+    # Set numerical parameters
+    mx = 10  # number of gridpoints in space
+    mt = 1000  # number of gridpoints in time
 
-# Set up the solution variables
-u_j = np.zeros(x.size)        # u at current time step
-u_jp1 = np.zeros(x.size)      # u at next time step
+    # Set up the numerical environment variables
+    x = np.linspace(0, L, mx + 1)  # mesh points in space
+    t = np.linspace(0, T, mt + 1)  # mesh points in time
+    deltax = x[1] - x[0]  # gridspacing in x
+    deltat = t[1] - t[0]  # gridspacing in t
+    lmbda = kappa * deltat / (deltax ** 2)  # mesh fourier number
+    print("deltax=", deltax)
+    print("deltat=", deltat)
+    print("lambda=", lmbda)
 
-# Set initial condition
-for i in range(0, mx+1):
-    u_j[i] = u_I(x[i])
+    if method == 'fw':
+        diagonals = [[lmbda] * (mx - 1), [1 - 2 * lmbda] * mx, [lmbda] * (mx - 1)]
+        A_FE = scipy.sparse.diags(diagonals, [-1, 0, 1]).toarray()
+        print(A_FE)
+    elif method == 'bw':
+        diagonals = [[- lmbda] * (mx - 1), [1 + 2 * lmbda] * mx, [- lmbda] * (mx - 1)]
+        A_BE = scipy.sparse.diags(diagonals, [-1, 0, 11], format='csc')
+    elif method == 'CN':
+        diagonals = [[-lmbda / 2] * (mx - 1), [1 + lmbda] * mx, [-lmbda / 2] * (mx - 1)]
+        A_CN = scipy.sparse.diags(diagonals, [-1, 0, 1], format='csc')
+        diagonals = [[lmbda / 2] * (mx - 1), [1 - lmbda] * mx, [lmbda / 2] * (mx - 1)]
+        B_CN = scipy.sparse.diags(diagonals, [-1, 0, 1], format='csc')
 
-# Solve the PDE: loop over all time points
-for j in range(0, mt):
-    # Forward Euler timestep at inner mesh points
-    # PDE discretised at position x[i], time t[j]
-    for i in range(1, mx):
-        u_jp1[i] = u_j[i] + lmbda*(u_j[i-1] - 2*u_j[i] + u_j[i+1])
+        # Set up the solution variables
+    u_j = np.zeros(x.size)  # u at current time step
+    u_jp1 = np.zeros(x.size)  # u at next time step
 
-    # Boundary conditions
-    u_jp1[0] = 0; u_jp1[mx] = 0
+    # Set initial condition
+    for i in range(0, mx + 1):
+        u_j[i] = u_I(x[i])
 
-    # Save u_j at time t[j+1]
-    u_j[:] = u_jp1[:]
+    # Solve the PDE: loop over all time points
+    for j in range(0, mt):
+        # Forward Euler timestep at inner mesh points
+        # PDE discretised at position x[i], time t[j]
+        if method == 'fw':
+            u_jp1[1:] = A_FE * u_j[1:]
+        elif method == 'bw':
+            u_jp1[1:] = spsolve(A_BE, u_j[1:])
+        elif method == 'CN':
+            u_jp1[1:] = spsolve(A_CN, B_CN * u_j[1:])
 
-# Plot the final result and exact solution
-pl.plot(x,u_j,'ro',label='num')
-xx = np.linspace(0,L,250)
-pl.plot(xx,u_exact(xx,T),'b-',label='exact')
-pl.xlabel('x')
-pl.ylabel('u(x,0.5)')
-pl.legend(loc='upper right')
-pl.show
+        # Boundary conditions
+        u_jp1[0] = 0;
+        u_jp1[mx] = 0
+
+        # Save u_j at time t[j+1]
+        u_j[:] = u_jp1[:]
+
+    # Plot the final result and exact solution
+    pl.plot(x, u_j, 'ro', label='num')
+    xx = np.linspace(0, L, 250)
+    pl.plot(xx, u_exact(xx, T), 'b-', label='exact')
+    pl.xlabel('x')
+    pl.ylabel('u(x,0.5)')
+    pl.legend(loc='upper right')
+    pl.show
