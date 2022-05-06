d1c90552bf364c46d754819e068af83edb6d8efd
rbromley19 <ua19949@bristol.ac.uk>
2022-05-07 00:40:26 +0100

    added hopf normal continuation and small amendments to the natural continuation function

--- num_continuation.py
+++ num_continuation.py
@@ -4,13 +4,6 @@ from scipy.optimize import fsolve
 from numerical_shooting import orbit_calc, num_shoot
 
 
-# def hopf_2(t, u, b):
-#     u1, u2 = u
-#     du1 = b * u1 - u2 - u1 * (u1 ** 2 + u2 ** 2)
-#     du2 = u1 + b * u2 - u2 * (u1 ** 2 + u2 ** 2)
-#     return np.array([du1, du2])
-
-
 def natural_continuation(f, u0, par, vary_par, range, space, discretisation):
     params = np.linspace(range[0], range[1], space)
     result = []
@@ -19,25 +12,22 @@ def natural_continuation(f, u0, par, vary_par, range, space, discretisation):
         counter = counter + 1
         print(counter)
         par[vary_par] = i
-        if discretisation == 'num_shoot':
-            sol = fsolve(lambda U, f: discretisation(U, f, var=0), u0, f)
-            result.append(sol)
-        else:
-            sol = fsolve(lambda U, f: discretisation(f), u0, f)
+        sol = fsolve(lambda U, f: discretisation(U, f, i), u0, f)
         result.append(sol)
     result = np.array(result)
+    print(result)
     return result
 
 
 def cubic_natural():
-    # params, sol_array = natural_continuation(cubic, [1], -2, 0, -2, 2, 30, lambda x: x, fsolve)
-    result = natural_continuation(cubic, 1.5, [2], 0, (-2, 2), 30, lambda x: x)
+    result = natural_continuation(cubic, 1.5, [2], 0, (-2, 2), 30, lambda U, f, par: f(U, par))
     print(result)
 
 
 def hopf_natural():
-    pass
+    natural_continuation(hopf_normal, [0.1, 0.5, 6], [2], 0, (2, 0), 30, lambda U, f, par: num_shoot(U, f, 0, par))
 
 
 if __name__ == '__main__':
     cubic_natural()
+    hopf_natural()
