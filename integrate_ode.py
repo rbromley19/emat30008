80f89501fc2b379b250dffb2393fc2309cf4981c
rbromley19 <ua19949@bristol.ac.uk>
2022-05-07 00:26:53 +0100

    added parameter to pass arguments for continuation

--- integrate_ode.py
+++ integrate_ode.py
@@ -1,9 +1,10 @@
 import numpy as np
 import math
+from ode_functions import ode_1, f_s, X_analytic
 
 
 # Function to calculate each step for euler integration method
-def euler_step(f, x0, t0, h):
+def euler_step(f, x0, t0, h, *args):
     """Calculates one Euler step for a function f given an initial x0 and t0 and step h
 
 Parameters
@@ -25,13 +26,13 @@ t0 : float
     Final t-value after a single Euler step
 """
 
-    x1 = x0 + h * np.array(f(t0, x0))
+    x1 = x0 + h * np.array(f(t0, x0, *args))
     t1 = t0 + h
     return x1, t1
 
 
 # Function to calculate each step for runge-kutta integration method
-def rk4_step(f, x0, t0, h):
+def rk4_step(f, x0, t0, h, *args):
     """Calculates one Runge-Kutta (4th order) step for a function f given an initial x0 and t0 and step h
 
 Parameters
@@ -52,18 +53,17 @@ x0 : float
 t0 : float
     Final t-value after a single Runge-Kutta step
 """
-
-    k1 = np.array(f(t0, x0))
-    k2 = np.array(f(t0 + h / 2, x0 + (h / 2) * k1))
-    k3 = np.array(f(t0 + h / 2, x0 + (h / 2) * k2))
-    k4 = np.array(f(t0 + h, x0 + h * k3))
+    k1 = np.array(f(t0, x0, *args))
+    k2 = np.array(f(t0 + h / 2, x0 + (h / 2) * k1, *args))
+    k3 = np.array(f(t0 + h / 2, x0 + (h / 2) * k2, *args))
+    k4 = np.array(f(t0 + h, x0 + h * k3, *args))
     x1 = x0 + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
     t1 = t0 + h
     return x1, t1
 
 
 # Function to solve ode(s) between x1t1 to x2t2 in steps less than dt_max
-def solve_to(method, f, x1, t1, t2, dt_max):
+def solve_to(method, f, x1, t1, t2, dt_max, *args):
     """Solves ODE from x1, t1 to x2, t2 while less than dt_max
 
 Parameters
@@ -94,15 +94,15 @@ x1 : float
 
     steps = math.floor((t2 - t1) / dt_max)
     for i in range(steps):
-        x1, t1 = func(f, x1, t1, dt_max)
+        x1, t1 = func(f, x1, t1, dt_max, *args)
     if t1 != t2:
         h = t2 - t1
-        x1, t1 = func(f, x1, t1, h)
+        x1, t1 = func(f, x1, t1, h, *args)
     return x1
 
 
 # Function to generate results from integration
-def solve_ode(f, x0, t, method, dt_max):
+def solve_ode(f, x0, t, method, dt_max, *args):
     """Generates and returns the solution estimates from integration
 
 Parameters
@@ -128,5 +128,20 @@ x : array
     x[0] = x0
 
     for i in range(1, len(t)):
-        x[i] = solve_to(method, f, x[i - 1], t[i - 1], t[i], dt_max)
+        x[i] = solve_to(method, f, x[i - 1], t[i - 1], t[i], dt_max, *args)
     return x
+
+
+def rk4_run(f, t):
+    method = 'rk4'
+    x0 = [1]
+    dt_max = 0.01
+    rk4 = solve_ode(f, x0, t, method, dt_max)
+    print('Runge-kutta approximation = ' + str(rk4))
+
+
+if __name__ == '__main__':
+    f = ode_1
+    true_sol = math.e
+    t = [0, 1]
+    rk4_run(f, t)
--- numerical_shooting.py
+++ numerical_shooting.py
@@ -5,7 +5,7 @@ from ode_functions import predator_prey
 import matplotlib.pyplot as plt
 
 
-def orbit_calc(f, u0, T, var):
+def orbit_calc(f, u0, var, *args):
     """Calculates the periodic orbit of ODEs using numerical shooting using fsolve
 
 Parameters
@@ -27,13 +27,13 @@ T  : float
     Time period of orbit
 """
 
-    U0 = np.append(u0, T)
-    orbit = fsolve(lambda U, f: num_shoot(U, f, var), U0, f)
+    # U0 = np.append(u0, T)
+    orbit = fsolve(lambda U, f: num_shoot(U, f, var, *args), u0, f)
     return orbit
 
 
 # Shooting function, implements the equation u0 - F(u0, T)
-def num_shoot(U, f, var):
+def num_shoot(U, f, var, *args):
     """Implements numerical shooting by calculating u0 - G(u0, T) and the phase conditions
 
 Parameters
@@ -50,18 +50,17 @@ Returns
 g : array
     System of equations of the boundary value problem to be solved
 """
-
     u0 = U[:-1]
     T = U[-1]
-    phase = conditions(f, u0, var)
-    integ_sol = solve_ode(f, u0, [0, T], 'rk4', 0.01)
+    phase = conditions(f, u0, var, *args)
+    integ_sol = solve_ode(f, u0, [0, T], 'rk4', 0.01, *args)
     diff = abs(u0 - integ_sol[-1])
     g = np.append((diff), phase)
     return g
 
 
 # Function to calculate phase condition using d[var]/dt(0) of a system of odes f
-def conditions(f, u0, var):
+def conditions(f, u0, var, *args):
     """Calculates the phase condition of the ODEs using f(0, u0)[var]
 
 Parameters
@@ -78,8 +77,7 @@ Returns
 phase_cond : array
             Phase condition for shooting
 """
-
-    phase_cond = np.array([f(0, u0)[var]])
+    phase_cond = np.array([f(0, u0, *args)[var]])
     return phase_cond
 
 
