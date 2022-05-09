19fa03f1680339b0ead8638957af35a4fcfbb580
rbromley19 <ua19949@bristol.ac.uk>
2022-05-09 11:05:51 +0100

    modularisation of PDE solver

--- solve_pde_V2.py
+++ solve_pde_V2.py
@@ -4,7 +4,6 @@
 #   u=0 at x=0,L, t>0
 # and prescribed initial temperature
 #   u=u_I(x) 0<=x<=L,t=0
-
 import numpy as np
 import pylab as pl
 from math import pi
@@ -15,6 +14,50 @@ from scipy.sparse.linalg import spsolve
 kappa = 1.0  # diffusion constant
 L = 1.0  # length of spatial domain
 T = 0.5  # total time to solve for
+
+
+def calc_PDE(L, T, kappa, method, mx=10, mt=1000):
+    if isinstance(mx, mt, kappa, float):
+        return env_variables(L, T, mx, mt, kappa)
+    else:
+        raise Exception('gridpoints in space/time need to be floats')
+    # Set initial condition
+    for i in range(0, mx + 1):
+        u_j[i] = u_I(x[i])
+    methods = {'fw': fw, 'bw': bw, 'ck': ck}
+    method = str(method)
+    if method in methods:
+        solve_PDE(mt, method, methods, lmbda, mx, u_j, u_jp1)
+    else:
+        raise Exception("Method %s not implemented" % method)
+
+
+
+def env_variables(L, T, kappa, mx=10, mt=1000):
+    # Set numerical parameters
+    x = np.linspace(0, L, mx + 1)  # mesh points in space
+    t = np.linspace(0, T, mt + 1)  # mesh points in time
+    deltax = x[1] - x[0]  # gridspacing in x
+    deltat = t[1] - t[0]  # gridspacing in t
+    # Set up the solution variables
+    u_j = np.zeros(x.size)  # u at current time step
+    u_jp1 = np.zeros(x.size)  # u at next time step
+    lmbda = kappa * deltat / (deltax ** 2)  # mesh fourier number
+    return x, t, deltax, deltat, u_j, u_jp1, lmbda
+
+
+def solve_PDE(mt, method, methods, lmbda, mx, u_j, u_jp1):
+    # Solve the PDE: loop over all time points
+    for j in range(0, mt):
+        methods[method](lmbda, mx, u_j, u_jp1)
+
+        # Boundary conditions
+        u_jp1[0] = 0;
+        u_jp1[mx] = 0
+
+        # Save u_j at time t[j+1]
+        u_j[:] = u_jp1[:]
+    return u_j
 
 
 def u_I(x):
@@ -43,7 +86,6 @@ def solve_PDE(u_I, kappa, L, T, mx, mt, method):
     print("deltax=", deltax)
     print("deltat=", deltat)
     print("lambda=", lmbda)
-
     # Set up the solution variables
     u_j = np.zeros(x.size)  # u at current time step
     u_jp1 = np.zeros(x.size)  # u at next time step
@@ -53,6 +95,33 @@ def solve_PDE(u_I, kappa, L, T, mx, mt, method):
         u_j[i] = u_I(x[i])
 
     # Solve the PDE: loop over all time points
+    for j in range(0, mt):
+        # Forward Euler timestep at inner mesh points
+        # PDE discretised at position x[i], time t[j]
+        # Check input method is valid, execute if so
+        methods = {'fw': fw, 'bw': bw, 'ck': ck}
+        method_name = str(method)
+        if method_name in methods:
+            methods[method_name](lmbda, mx, u_j, u_jp1)
+        else:
+            raise Exception("Method %s not implemented" % method_name)
+
+        # Boundary conditions
+        u_jp1[0] = 0;
+        u_jp1[mx] = 0
+
+        # Save u_j at time t[j+1]
+        u_j[:] = u_jp1[:]
+    plot(x, u_j, L, T)
+
+
+def int_conds(mx, u_j, x):
+    for i in range(0, mx + 1):
+        u_j[i] = u_I(x[i])
+    return u_j[i]
+
+
+def solve_PDE(method, mt, lmbda, mx, u_j, u_jp1, x):
     for j in range(0, mt):
         # Forward Euler timestep at inner mesh points
         # PDE discretised at position x[i], time t[j]
@@ -107,4 +176,5 @@ def plot(x, u_j, L, T):
     pl.show()
 
 
-solve_PDE(u_I, 1, 1, 0.5, 10, 1000, 'ck')
+if __name__ == '__main__':
+    solve_PDE(u_I, 1, 1, 0.5, 10, 1000, 'fw')
