1bede7b346b7b434bb9b0f1804b861693d8e9f07
rbromley19 <ua19949@bristol.ac.uk>
2022-05-10 21:32:43 +0100

    fixing PDEs and starting to attempt neumann BCs

--- solve_pde_V2.py
+++ solve_pde_V2.py
@@ -24,7 +24,7 @@ def u_exact(x, t):
     return y
 
 
-def calc_PDE(u_I, kappa, L, T, method, bound_funcs, bc):
+def calc_PDE(u_I, kappa, L, T, method, bound, bc):
     x, lmbda, mx, mt = init_params(L, T, kappa)
     u_j, u_jp1 = sol_vars(x)
 
@@ -33,9 +33,9 @@ def calc_PDE(u_I, kappa, L, T, method, bound_funcs, bc):
         u_j[i] = u_I(x[i])
     u_j[0] = 0
     u_j[mx] = 0
-    u_j = solve_PDE(lmbda, mx, u_j, mt, method, bound_funcs, x, bc)
+    u_j = solve_PDE(lmbda, mx, u_j, u_jp1, mt, method, bound, x, bc)
     print(u_j)
-    plot(x, u_j, L, T)
+    plot(x, u_j, L, T, bc)
 
 
 def init_params(L, T, kappa):
@@ -62,43 +62,42 @@ def sol_vars(x):
     return u_j, u_jp1
 
 
-def solve_PDE(lmbda, mx, u_j, mt, method, bound_funcs, x, bc):
+def solve_PDE(lmbda, mx, u_j, u_jp1, mt, method, bound, x, bc):
     # Solve the PDE: loop over all time points
     methods = {'fw': fw, 'bw': bw, 'ck': ck}
     method_name = str(method)
     if method_name in methods:
-        u_j = methods[method_name](lmbda, mx, u_j, mt, bound_funcs, bc)
+        if method_name == 'fw':
+            u_j = methods[method_name](lmbda, mx, u_j, mt, bound, bc)
+        else:
+            u_j = methods[method_name](lmbda, mx, mt, u_j)
     else:
         raise Exception("Method %s not implemented" % method_name)
-
-    print(x, u_j)
     return x, u_j
 
 
-def fw(lmbda, mx, u_j, mt, bound_funcs, bc):
-    # diagonals = [[lmbda] * (mx - 1), [1 - 2 * lmbda] * mx, [lmbda] * (mx - 1)]
-    # A_FW = scipy.sparse.diags(diagonals, [-1, 0, 1]).toarray()
+def fw(lmbda, mx, u_j, mt, bound, bc):
     n = np.around(mx - 1)
-    diagonals = np.array([lmbda * np.ones(n - 1), np.ones(n) - 2 * lmbda, lmbda * np.ones(n - 1)], dtype=np.dtype(object))
+    diagonals = np.array([lmbda * np.ones(n - 1), np.ones(n) - 2 * lmbda, lmbda * np.ones(n - 1)],
+                         dtype=np.dtype(object))
     A_FW = scipy.sparse.diags(diagonals, [-1, 0, 1]).toarray()
     for i in range(mt):
-        if bc == 'zero':
-            u_j = np.dot(A_FW, u_j[1:])
-            # u_jp1[1:] = np.dot(A_FW, u_j[1:])
-        u_j0 = u_j[1:mx]
-        if bc == 'dirichlet':
-            u_j = calc_diri(u_j0, A_FW, lmbda, mt, bound_funcs)
-
+        bc_list = {'dirichlet': dirichlet, 'neumann': neumann, 'none': none}
+        bc_name = str(bc)
+        if bc_name in bc_list:
+            u_j0 = u_j[1:mx]
+            u_j = bc_list[bc_name](lmbda, u_j0, mt, bound, A_FW)
+        else:
+            return Exception('Boundary condition %s not implemented. Select a valid boundary condition.' % bc)
     return u_j
 
 
-def calc_diri(u_j0, A_FW, lmbda, mt, bound_funcs):
+def dirichlet(lmbda, u_j0, mt, bound, A_FW):
     sol_vec = np.zeros(len(u_j0))
-    p, q = bound_funcs
+    p, q = bound
     sol_vec[0] = p(mt)
     sol_vec[-1] = q(mt)
     sol = A_FW.dot(u_j0) + lmbda * np.array(sol_vec)
-    # Add boundary values
     u_j = [p(mt)]
     for i in sol:
         u_j.append(i)
@@ -106,11 +105,32 @@ def calc_diri(u_j0, A_FW, lmbda, mt, bound_funcs):
     return u_j
 
 
-def bw(lmbda, mx, u_j, u_jp1):
-    diagonals = [[- lmbda] * (mx - 1), [1 + 2 * lmbda] * mx, [- lmbda] * (mx - 1)]
-    A_BW = scipy.sparse.diags(diagonals, [-1, 0, 1], format='csc')
-    u_jp1[1:] = spsolve(A_BW, u_j[1:])
-    return u_jp1[1:]
+def none(u_j0, A_FW, lmbda, mt, bound):
+    u_j = [0]
+    sol = A_FW.dot(u_j0)
+    for i in sol:
+        u_j.append(i)
+    u_j.append(0)
+    return u_j
+
+
+def neumann():
+    pass
+
+
+def bw(lmbda, mx, mt, u_j):
+    n = np.around(mx - 1)
+    diagonals = np.array([-lmbda * np.ones(n - 1), 2 * lmbda + np.ones(n), -lmbda * np.ones(n - 1)],
+                         dtype=np.dtype(object))
+    A = scipy.sparse.diags(diagonals, [-1, 0, 1], format='csr')
+    for i in range(mt):
+        u_j0 = u_j[1:mx]
+        sol = spsolve(A, u_j0)
+        u_j = [0]
+        for i in sol:
+            u_j.append(i)
+        u_j.append(0)
+    return u_j
 
 
 def ck(lmbda, mx, u_j, u_jp1):
@@ -122,12 +142,13 @@ def ck(lmbda, mx, u_j, u_jp1):
     return u_jp1[1:]
 
 
-def plot(x, u_j, L, T):
+def plot(x, u_j, L, T, bc):
     # Plot the final result and exact solution
     print(u_j)
     pl.plot(x, u_j[1], 'ro', label='num')
     xx = np.linspace(0, L, 250)
-    # pl.plot(xx, u_exact(xx, T), 'b-', label='exact')
+    if bc is None:
+        pl.plot(xx, u_exact(xx, T), 'b-', label='exact')
     pl.xlabel('x')
     pl.ylabel('u(x,0.5)')
     pl.legend(loc='upper right')
@@ -147,4 +168,5 @@ kappa = 1.0  # diffusion constant
 L = 1.0  # length of spatial domain
 T = 0.5  # total time to solve for
 
-calc_PDE(u_I, kappa, L, T, 'fw', (p_func, q_func), 'dirichlet')
+# calc_PDE(u_I, kappa, L, T, 'fw', (p_func, q_func), 'dirichlet')
+calc_PDE(u_I, kappa, L, T, 'bw', None, None)
