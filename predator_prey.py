57613929c3ca3f8cfeac108e5dabda9a6e90d96a
rbromley19 <ua19949@bristol.ac.uk>
2022-05-10 13:02:17 +0100

    better simulation of PP equations

--- predator_prey.py
+++ predator_prey.py
@@ -1,59 +1,68 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import solve_ivp
-from matplotlib import pyplot
 from integrate_ode import solve_ode
-from scipy.optimize import root
-from math import nan
-from ode_functions import pred_prey
 
-b = 0.2
-int = [0.25, 0.25]
-t = np.linspace(0, 150, 1501)
-bspace = np.linspace(0.1, 0.5, 5)
 
-sol = solve_ode(lambda t, u: pred_prey(t, u, b), int, t, 'rk4', 0.001)
-x = sol[:, 0]
-y = sol[:, 1]
-pyplot.plot(x, y)
-pyplot.show()
+def predator_prey_below(t, u, b=0.22):
+    """Function to simulate the predator-prey (Lokterra-Volterra Equations) with b<0.26
 
-# X-nullcline
-Xval = np.linspace(0.1, 0.6, 100)
-Yval = np.zeros(np.size(Xval))
-for (i, X) in enumerate(Xval):
-    result = root(lambda N: pred_prey(nan, (X, N), b)[0], 0.25)
-    if result.success:
-        Yval[i] = result.x
-    else:
-        Yval[i] = nan
-pyplot.plot(Xval, Yval)
+Parameters
+----------
+t  : float
+     Time
+u  : array
+     x-value at time t
+b  : float
+     value of constant 'b'
+Returns
+-------
+Array of dx/dt and dy/dt
+"""
+    a = 1
+    d = 0.1
+    x, y = u
+    dx = x * (1 - x) - (a * x * y) / (d + x)
+    dy = b * y * (1 - (y / x))
+    return np.array((dx, dy))
 
-# Y-nullcline
-Xval = np.linspace(0.1, 0.6, 100)
-Yval = np.zeros(np.size(Xval))
-for (i, X) in enumerate(Xval):
-    result = root(lambda N: pred_prey(nan, (X, N), b)[1], 0.25)
-    if result.success:
-        Yval[i] = result.x
-    else:
-        Yval[i] = nan
-pyplot.plot(Xval, Yval)
-pyplot.show()
 
-# %%
-result = root(lambda u: pred_prey(nan, u, b), (0.25, 0.25))
-if result.success:
-    print("Equilibrium at {}".format(result.x))
-else:
-    print("Failed to converge")
-result = root(lambda u: pred_prey(nan, u, b), (0.5, 0.3))
-if result.success:
-    print("Equilibrium at {}".format(result.x))
-else:
-    print("Failed to converge")
-# result = root(lambda u: pred_prey(nan, u, b), np.array((0.4, 0.5)))
-# if result.success:
-#     print("Equilibrium at {}".format(result.x))
-# else:
-#     print("Failed to converge")
+def predator_prey_above(t, u, b=0.3):
+    """Function to simulate the predator-prey (Lokterra-Volterra Equations) with b>0.26
+
+Parameters
+----------
+t  : float
+     Time
+u  : array
+     x-value at time t
+b  : float
+     value of constant 'b'
+Returns
+-------
+Array of dx/dt and dy/dt
+"""
+    a = 1
+    d = 0.1
+    x, y = u
+    dx = x * (1 - x) - (a * x * y) / (d + x)
+    dy = b * y * (1 - (y / x))
+    return np.array((dx, dy))
+
+
+def simulate_plot(f):
+    t = np.linspace(0, 200, 1000)
+    x0 = [0.24, 0.24]
+    sol = solve_ode(f, x0, t, 'rk4', 0.01, array=True)
+    plt.plot(t, sol[0], label='x(t)')
+    plt.plot(t, sol[1], label='y(t)')
+    plt.xlabel('t')
+    plt.ylabel('x(t) and y(t)')
+    plt.legend(loc='best')
+    plt.show()
+
+
+if __name__ == '__main__':
+    # Simulate Predator-Prey for b<0.26
+    simulate_plot(lambda t, u: predator_prey_below(t, u))
+    # Simulate Predator-Prey for b>0.26
+    simulate_plot(lambda t, u: predator_prey_above(t, u))
