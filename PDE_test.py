eeaa52e908b1687dbe817668987bd9fa0a790c32
rbromley19 <ua19949@bristol.ac.uk>
2022-05-10 23:15:28 +0100

    moved PDE running and plotting to dedicated file (PDE_test.py)

---
+++ PDE_test.py
@@ -0,0 +1,61 @@
+from solve_pde_V2 import calc_PDE
+from math import pi
+import numpy as np
+import pylab as pl
+
+
+def u_I(x):
+    # initial temperature distribution
+    y = np.sin(pi * x / L)
+    return y
+
+
+def u_exact(x, t):
+    # the exact solution
+    y = np.exp(-kappa * (pi ** 2 / L ** 2) * t) * np.sin(pi * x / L)
+    return y
+
+
+def p_func(t):
+    return t
+
+
+def q_func(t):
+    return t
+
+
+def finite_diff_run():
+    xfw, u_jfw = calc_PDE(u_I, kappa, L, T, 'fw', None, None)
+    xbw, u_jbw = calc_PDE(u_I, kappa, L, T, 'bw', None, None)
+    xck, u_jck = calc_PDE(u_I, kappa, L, T, 'ck', None, None)
+    # Plot the final result and exact solution
+    pl.plot(xfw, u_jfw[1], 'r-o', label='Forward-Euler')
+    pl.plot(xbw, u_jbw[1], 'g-o', label='Backward-Euler')
+    pl.plot(xck, u_jck[1], 'y-o', label='Crank-Nicholson')
+    xx = np.linspace(0, L, 250)
+    pl.plot(xx, u_exact(xx, T), 'b-', label='Exact')
+    pl.title('Plot of Finite Difference Methods')
+    pl.xlabel('x')
+    pl.ylabel('u(x,0.5)')
+    pl.legend(loc='upper right')
+    pl.show()
+
+
+def dirichlet_run():
+    x, u_j = calc_PDE(u_I, kappa, L, T, 'fw', (p_func, q_func), 'dirichlet')
+    pl.plot(x, u_j[1], 'r-o', label='Forward Euler with Dirichlet boundary conditions')
+    pl.title('Plot of Forward Euler with Dirichlet Boundary Conditions')
+    pl.xlabel('x')
+    pl.ylabel('u(x,0.5)')
+    pl.legend(loc='upper right')
+    pl.show()
+
+
+if __name__ == '__main__':
+    # Set problem parameters/functions
+    kappa = 1.0  # diffusion constant
+    L = 1.0  # length of spatial domain
+    T = 0.5  # total time to solve for
+
+    finite_diff_run()
+    dirichlet_run()
--- solve_pde_V2.py
+++ solve_pde_V2.py
@@ -1,27 +1,6 @@
-# simple forward Euler solver for the 1D heat equation
-#   u_t = kappa u_xx  0<x<L, 0<t<T
-# with zero-temperature boundary conditions
-#   u=0 at x=0,L, t>0
-# and prescribed initial temperature
-#   u=u_I(x) 0<=x<=L,t=0
-
 import numpy as np
-import pylab as pl
-from math import pi
 import scipy
 from scipy.sparse.linalg import spsolve
-
-
-def u_I(x):
-    # initial temperature distribution
-    y = np.sin(pi * x / L)
-    return y
-
-
-def u_exact(x, t):
-    # the exact solution
-    y = np.exp(-kappa * (pi ** 2 / L ** 2) * t) * np.sin(pi * x / L)
-    return y
 
 
 def calc_PDE(u_I, kappa, L, T, method, bound, bc):
@@ -33,9 +12,8 @@ def calc_PDE(u_I, kappa, L, T, method, bound, bc):
         u_j[i] = u_I(x[i])
     u_j[0] = 0
     u_j[mx] = 0
-    u_j = solve_PDE(lmbda, mx, u_j, u_jp1, mt, method, bound, x, bc)
-    print(u_j)
-    plot(x, u_j, L, T, bc)
+    u_j = solve_PDE(lmbda, mx, u_j, mt, method, bound, x, bc)
+    return x, u_j
 
 
 def init_params(L, T, kappa):
@@ -62,7 +40,7 @@ def sol_vars(x):
     return u_j, u_jp1
 
 
-def solve_PDE(lmbda, mx, u_j, u_jp1, mt, method, bound, x, bc):
+def solve_PDE(lmbda, mx, u_j, mt, method, bound, x, bc):
     # Solve the PDE: loop over all time points
     methods = {'fw': fw, 'bw': bw, 'ck': ck}
     method_name = str(method)
@@ -82,33 +60,36 @@ def fw(lmbda, mx, u_j, mt, bound, bc):
                          dtype=np.dtype(object))
     A_FW = scipy.sparse.diags(diagonals, [-1, 0, 1]).toarray()
     for i in range(mt):
-        bc_list = {'dirichlet': dirichlet, 'neumann': neumann, 'none': none}
+        bc_list = {'dirichlet': dirichlet, 'neumann': neumann}
         bc_name = str(bc)
         if bc_name in bc_list:
             u_j0 = u_j[1:mx]
             u_j = bc_list[bc_name](lmbda, u_j0, mt, bound, A_FW)
+        elif bc is None:
+            u_j0 = u_j[1:mx]
+            u_j = none(u_j0, A_FW)
         else:
             return Exception('Boundary condition %s not implemented. Select a valid boundary condition.' % bc)
     return u_j
 
 
 def dirichlet(lmbda, u_j0, mt, bound, A_FW):
-    sol_vec = np.zeros(len(u_j0))
+    sol_vec = np.array(np.zeros(len(u_j0)))
     p, q = bound
     sol_vec[0] = p(mt)
     sol_vec[-1] = q(mt)
-    sol = A_FW.dot(u_j0) + lmbda * np.array(sol_vec)
+    sol_arr = A_FW.dot(u_j0) + lmbda * sol_vec
     u_j = [p(mt)]
-    for i in sol:
+    for i in sol_arr:
         u_j.append(i)
     u_j.append(q(mt))
     return u_j
 
 
-def none(u_j0, A_FW, lmbda, mt, bound):
+def none(u_j0, A_FW):
     u_j = [0]
-    sol = A_FW.dot(u_j0)
-    for i in sol:
+    sol_arr = A_FW.dot(u_j0)
+    for i in sol_arr:
         u_j.append(i)
     u_j.append(0)
     return u_j
@@ -122,51 +103,30 @@ def bw(lmbda, mx, mt, u_j):
     n = np.around(mx - 1)
     diagonals = np.array([-lmbda * np.ones(n - 1), 2 * lmbda + np.ones(n), -lmbda * np.ones(n - 1)],
                          dtype=np.dtype(object))
-    A = scipy.sparse.diags(diagonals, [-1, 0, 1], format='csr')
+    A_BW = scipy.sparse.diags(diagonals, [-1, 0, 1], format='csr')
     for i in range(mt):
         u_j0 = u_j[1:mx]
-        sol = spsolve(A, u_j0)
+        sol_arr = spsolve(A_BW, u_j0)
         u_j = [0]
-        for i in sol:
+        for i in sol_arr:
             u_j.append(i)
         u_j.append(0)
     return u_j
 
 
-def ck(lmbda, mx, u_j, u_jp1):
-    diagonals = [[-lmbda / 2] * (mx - 1), [1 + lmbda] * mx, [-lmbda / 2] * (mx - 1)]
-    A_CK = scipy.sparse.diags(diagonals, [-1, 0, 1], format='csc')
-    diagonals = [[lmbda / 2] * (mx - 1), [1 - lmbda] * mx, [lmbda / 2] * (mx - 1)]
-    B_CK = scipy.sparse.diags(diagonals, [-1, 0, 1], format='csc')
-    u_jp1[1:] = spsolve(A_CK, B_CK * u_j[1:])
-    return u_jp1[1:]
-
-
-def plot(x, u_j, L, T, bc):
-    # Plot the final result and exact solution
-    print(u_j)
-    pl.plot(x, u_j[1], 'ro', label='num')
-    xx = np.linspace(0, L, 250)
-    if bc is None:
-        pl.plot(xx, u_exact(xx, T), 'b-', label='exact')
-    pl.xlabel('x')
-    pl.ylabel('u(x,0.5)')
-    pl.legend(loc='upper right')
-    pl.show()
-
-
-def p_func(t):
-    return t
-
-
-def q_func(t):
-    return t
-
-
-# Set problem parameters/functions
-kappa = 1.0  # diffusion constant
-L = 1.0  # length of spatial domain
-T = 0.5  # total time to solve for
-
-# calc_PDE(u_I, kappa, L, T, 'fw', (p_func, q_func), 'dirichlet')
-calc_PDE(u_I, kappa, L, T, 'bw', None, None)
+def ck(lmbda, mx, mt, u_j):
+    n = round(mx - 1)
+    diagonals = np.array([(-lmbda / 2) * np.ones(n - 1), lmbda + np.ones(n), (-lmbda / 2) * np.ones(n - 1)],
+                         dtype=np.dtype(object))
+    A_CK = scipy.sparse.diags(diagonals, [-1, 0, 1], format='csr')
+    diagonals = np.array([(lmbda / 2) * np.ones(n - 1), np.ones(n) - lmbda, (lmbda / 2) * np.ones(n - 1)],
+                         dtype=np.dtype(object))
+    B_CK = scipy.sparse.diags(diagonals, [-1, 0, 1], format='csr')
+    for i in range(mt):
+        u_j0 = u_j[1:mx]
+        sol_arr = spsolve(A_CK, B_CK.dot(u_j0))
+        u_j = [0]
+        for i in sol_arr:
+            u_j.append(i)
+        u_j.append(0)
+    return u_j
